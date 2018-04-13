8211ae293b94dd264aa83114e2b2e4d257115d97
IvanHomonets <32175155+IvanHomonets@users.noreply.github.com>
2018-04-13 18:39:33 +0300

    Printing letters, which are double

---
+++ task_2.py
@@ -0,0 +1,14 @@
+example_str = "agcdcgia"
+print(example_str)
+
+
+def filter_1(some_str):
+    filter_letters = []
+    for letter in some_str:
+        if some_str.count(letter) > 1 and letter not in filter_letters:
+            filter_letters.append(letter)
+    filter_str = ''.join(filter_letters)
+    return filter_str
+
+f = filter_1(example_str)
+print(f)
