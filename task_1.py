2c31322d8a3a383d26d41ca3146a2ae1c8e70909
IvanHomonets <32175155+IvanHomonets@users.noreply.github.com>
2018-04-16 13:17:24 +0300

    Change complexity
    I have changed complexity of it aalgorithm to O(n)

--- task_1.py
+++ task_1.py
@@ -1,10 +1,14 @@
-example_list = [1, 2, 3, 4, 5]
+example_list = [1, 2, 3, 4, 5, 6]
 
+
+# Now max complexity is O(n) because it depends on the length of list
 def my_reverse(some_list):
-    reverse_list = []
-    for i in some_list:
-        reverse_list.insert(0, i)
-    return reverse_list
+    for i in range(len(some_list)//2):
+        print(i)
+        start_point = i
+        end = -i-1
+        some_list[start_point], some_list[end] = some_list[end], some_list[start_point]
+    return some_list
 
 
 f = my_reverse(example_list)
