99300b85289b2af3593708c2528dad9c0b636dbd
IvanHomonets <32175155+IvanHomonets@users.noreply.github.com>
2018-04-16 20:01:29 +0300

    Make easier'

--- task_2.py
+++ task_2.py
@@ -1,14 +1,15 @@
 example_str = "agcdcgia"
+#ex = [1, 1, 3, 4, 6, 6]
 print(example_str)
 
 
 def filter_1(some_str):
-    filter_letters = []
-    for letter in some_str:
-        if some_str.count(letter) > 1 and letter not in filter_letters:
-            filter_letters.append(letter)
-    filter_str = ''.join(filter_letters)
+    filter_str = ''
+    for i in some_str:
+        if i not in filter_str and some_str.count(i) > 1:
+            filter_str += i
     return filter_str
+
 
 f = filter_1(example_str)
 print(f) # print 'agc'
