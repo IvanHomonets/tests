e4f50a9d6eba4d6e7ebbdefb2d25574829c8dae9
IvanHomonets <32175155+IvanHomonets@users.noreply.github.com>
2018-04-16 20:35:57 +0300

    All data is immtable

--- task_3.py
+++ task_3.py
@@ -8,10 +8,9 @@ def update(id, meaning):
     for i in range(len(array)):
         get_id = array[i].get('id')
         if get_id == id:
-            array[i]['state'] = meaning
-            print(get_id)
+            array[i] = {'id': id, 'state': meaning}
     return array
 
 
-f = update(7, True)
+f = update(2, False)
 print(f)
